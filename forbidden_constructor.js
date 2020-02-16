264ec19fabb39c52f523ea730890c1bb538e7c2d
Petr Penicka <petr.penicka@gmail.com>
2020-02-16 15:21:18 +0100

    Added solution to kata Forbidden Constructor

---
+++ forbidden_constructor.js
@@ -0,0 +1,29 @@
+// https://www.codewars.com/kata/551e467e24b7a4e0b20001e8
+
+var Point = (function () {
+  var inside = false;
+
+  var Point = function (x, y) {
+      if (!inside) {
+         throw new Error('Constructor cannot be called directly. Use Point.create(x, y) instead.');
+      }
+
+      inside = false;
+      this.x = x;
+      this.y = y;
+  };
+
+  Point.create = function (x, y) {
+      inside = true;
+      return new Point(x, y);
+  };
+
+  return Point;
+})();
+
+
+// TESTS
+var pt = Point.create(0, 0);
+console.log(pt.x === 0);
+console.log(pt.y === 0);
+console.log(pt instanceof Point);
