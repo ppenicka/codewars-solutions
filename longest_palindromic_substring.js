ad2c23f2b73372899ef034d3f57805b3b05c3962
Petr Penicka <petr.penicka@gmail.com>
2020-05-24 15:11:01 +0200

    first try of Manacher's algo, failing small amount of tests

--- longest_palindromic_substring.js
+++ longest_palindromic_substring.js
@@ -10,19 +10,32 @@ function longest_palindrome (s) {
   let centerIndex = 0;
   let leftIndex = 0;
   let rightIndex = 0;
-  let lengthsAtIndex = [0];
+  let expansionAtIndex = [0];
 
-  for (let i = 1; i < arr.length; i++) {
-    let expansion = 1;
-    lengthsAtIndex[i] = 0;
+  for (let i = 0; i < arr.length; i++) {
+    console.log("functionlongest_palindrome -> expansionAtIndex", expansionAtIndex)
+
+    let expansion = 0;
+    expansionAtIndex[i] = 0;
+
+    if (i <= rightIndex) {
+      let mirrorIndex = 2*centerIndex - rightIndex;
+      if (mirrorIndex - expansionAtIndex[mirrorIndex] >= leftIndex) {
+        expansion = expansionAtIndex[mirrorIndex];
+        expansionAtIndex[i] = expansion;
+      } else {
+        expansion = rightIndex - i;
+        expansionAtIndex[i] = expansion;
+      }
+    }
 
     while ((i - expansion >= 0) && (i + expansion < arr.length)) {
       if (arr[i - expansion] === arr[i + expansion]) {
-        if (lengthsAtIndex[centerIndex] < expansion) {
+        if (expansionAtIndex[centerIndex] < expansion) {
           centerIndex = i;
           leftIndex = i - expansion;
           rightIndex = i + expansion;
-          lengthsAtIndex[i] = expansion;
+          expansionAtIndex[i] = expansion;
         }
         expansion ++;
       } else break
@@ -37,14 +50,15 @@ function longest_palindrome (s) {
 
 
 // TESTS
-console.log(longest_palindrome('babad') === 'bab');
-console.log(longest_palindrome('madam') === 'madam');
-console.log(longest_palindrome('dde') === 'dd');
-console.log(longest_palindrome('ababbab') === 'babbab');
-console.log(longest_palindrome('abababa') === 'abababa');
-console.log(longest_palindrome('banana') === 'anana');
-console.log(longest_palindrome('abba') === 'abba');
-console.log(longest_palindrome('cbbd') === 'bb');
-console.log(longest_palindrome('zz') === 'zz');
-console.log(longest_palindrome('dddd') === 'dddd');
-console.log(longest_palindrome('') === '');
+// console.log(longest_palindrome('babad') === 'bab');
+// console.log(longest_palindrome('madam') === 'madam');
+// console.log(longest_palindrome('dde') === 'dd');
+// console.log(longest_palindrome('ababbab') === 'babbab');
+// console.log(longest_palindrome('abababa') === 'abababa');
+// console.log(longest_palindrome('banana') === 'anana');
+// console.log(longest_palindrome('abba') === 'abba');
+// console.log(longest_palindrome('cbbd') === 'bb');
+// console.log(longest_palindrome('zz') === 'zz');
+// console.log(longest_palindrome('dddd') === 'dddd');
+// console.log(longest_palindrome('') === '');
+console.log(longest_palindrome('yytyyffyfftyyyytffyfyfyyftfyfftffttyfyyyfftfffttyftttttftyfttytftytytyfftfftttffffyttytfttttytyyftyftffytfttfftfyytyytftttfffffyyyfyttyffyytttftftftfftyfyyytftyyffyffytyttytyytyyffffytfttyyyffttytttyfyyyffyffftyyyfttttyfyytyyfytyfttffyytyffftttfftffyftttyftffyfffyttttfyyffyffyfytffttttfyftfyyyfftfyttfffttyyttfytfffftyyfttytfttyfyfyfftfytffffttfytyfttyttyytttftyfyyyyyfyytytyttftfyftfttty') === 'ttyytyytt');
